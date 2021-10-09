c8e613a67ec1ba24ce7f4cd982ad6a1390e3872e
GalinaMonitor <gmonitor@student.21-school.ru>
2021-10-09 13:45:26 -0400

    Update base command

--- api/management/commands/update_base.py
+++ api/management/commands/update_base.py
@@ -5,18 +5,20 @@ from ...models import InstaUser
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		bot = Bot()
-		bot.login(username = "gmonitor21",  password = "53#NzAoX4S$DG5o$", use_cookie = False)
-		user_followers = bot.get_user_followers('katyushka_tyan')
+		bot.login(username = "",  password = "", proxy='', use_cookie = False)
+		user_followers = bot.get_user_followers('dmitriy.sa')
 		for follower in user_followers:
-			user = InstaUser()
-			user.user_id = follower
-			user.user_follower = 'katyushka_tyan'
-			user.user_login = bot.get_username_from_user_id(follower)
-			user.save()
-		user_followers = bot.get_user_followers('dima_gordey')
+			if not InstaUser.objects.filter(user_id = follower).exists():
+				user = InstaUser()
+				user.user_id = follower
+				user.user_follower = 'katyushka_tyan'
+				user.user_login = bot.get_username_from_user_id(follower)
+				user.save()
+		user_followers = bot.get_user_followers('dmitriy.sa')
 		for follower in user_followers:
-			user = InstaUser()
-			user.user_id = follower
-			user.user_follower = 'dima_gordey'
-			user.user_login = bot.get_username_from_user_id(follower)
-			user.save()
+			if not InstaUser.objects.filter(user_id = follower).exists():
+				user = InstaUser()
+				user.user_id = follower
+				user.user_follower = 'dima_gordey'
+				user.user_login = bot.get_username_from_user_id(follower)
+				user.save()
